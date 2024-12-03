2ef42a190e79ada2b21eb66a74640afec67526d9
hudamnhd <muhamadhuda519@gmail.com>
2024-12-03 12:44:15 +0700

    - netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -19,6 +19,7 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    netlifyPlugin(), 
     tsconfigPaths(),
   ],
 });
